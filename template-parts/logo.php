aed9dbc26ebb54cf4778b31fe8dc1522ac9a0329
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-05-18 20:41:52 -0300

    refactor: add logo replace

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -6,5 +6,5 @@ $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 <?php if (has_custom_logo()) {
     echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . " logo" . '">';
 } else {
-    echo '<h1>' . get_bloginfo('name') . ' </h1>';
+    echo '<img src="/wp-content/themes/hades/assets/images/Hades-Logo.png" alt="' . get_bloginfo('name') . " logo" . '">';
 } ?>
